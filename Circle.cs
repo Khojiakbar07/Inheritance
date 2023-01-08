29b068f80b2c50415bdf1cd55643d0760baa6769
Hojiakbar Zokirov <108798105+Khojiakbar07@users.noreply.github.com>
2023-01-08 14:35:14 +0500

    work with Inheritanse

---
+++ Circle.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project
+{
+    internal class Circle
+    {
+        protected double radius;
+
+    }
+}
---
+++ Shape.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project
+{
+    internal class Shape
+    {
+        protected Location c;
+
+        public string ToString()
+        {
+            return string.Empty;
+        }
+        public double Area()
+        {
+            return 0.0;
+        }
+        public double Perimeter()
+        {
+            return 0.0;
+        }
+    }
+}
